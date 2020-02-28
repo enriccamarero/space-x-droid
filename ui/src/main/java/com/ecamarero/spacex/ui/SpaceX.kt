ce1192a2054503f1f1706929e97eeb770b61cb2a
CAMARERO FLORES Enric <enriccamarero@gmail.com>
2020-02-28 12:41:47 +0100

    Initial Commit

---
+++ ui/src/main/java/com/ecamarero/spacex/ui/SpaceX.kt
@@ -0,0 +1,12 @@
+package com.ecamarero.spacex.ui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class SpaceX : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_space_x)
+    }
+}
